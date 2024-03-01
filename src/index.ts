4d2b778d7962d5c73b733e7b107fe46fa27b4369
Thomas Humphreys <thomasehumphreys@gmail.com>
2024-02-29 17:53:33 -0700

    chore: prettier & tsc config

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,14 @@
-import { BotConfig, newBotConfig, run } from "@xmtp/bot-kit-pro"
+import { BotConfig, newBotConfig, run } from '@xmtp/bot-kit-pro';
 
-import echo from "./echo"
+import echo from './echo';
 
 const defaultConfig: Partial<BotConfig> = {
-  xmtpEnv: "dev",
-}
+  xmtpEnv: 'dev',
+};
 
 const start = async () => {
-  const bots = [
-    newBotConfig("echo", defaultConfig, echo)
-  ]
-  await run(bots)
-}
+  const bots = [newBotConfig('echo', defaultConfig, echo)];
+  await run(bots);
+};
 
-start()
+start();
