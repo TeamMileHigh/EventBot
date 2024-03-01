fa81a3b83e6347051376869721f1b33b0dc34918
Thomas Humphreys <thomasehumphreys@gmail.com>
2024-03-01 15:23:41 -0700

    feat: private key and node env

--- src/Client.ts
+++ src/Client.ts
@@ -1,21 +1,27 @@
-import "./Environment.js"
-
-import { Client } from "@xmtp/xmtp-js"
-import dotenv from "dotenv"
-import { Wallet } from "ethers"
-dotenv.config()
+import { Client } from '@xmtp/xmtp-js';
+import dotenv from 'dotenv';
+import { Wallet } from 'ethers';
+dotenv.config();
 
 export default async function createClient(): Promise<Client> {
-  const key = Wallet.createRandom().privateKey
-  const wallet = new Wallet(
-    key,
-  )
+  let privateKey;
 
+  if (process.env.NODE_ENV === 'dev') {
+    privateKey = Wallet.createRandom().privateKey;
+  } else {
+    privateKey = process.env.CLIENT_PRIVATE_KEY;
+    if (!privateKey) {
+      throw new Error(
+        'CLIENT_PRIVATE_KEY environment variable not set for production.'
+      );
+    }
+  }
+
+  const wallet = new Wallet(privateKey);
   const client = await Client.create(wallet, {
-    env: "dev",
-  })
+    env: process.env.NODE_ENV as 'dev' | 'local' | 'production',
+  });
+  await client.publishUserContact();
 
-  await client.publishUserContact()
-
-  return client
+  return client;
 }
