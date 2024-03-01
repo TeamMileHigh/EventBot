4716623d338d7241727979408570d6cc20094fad
Steve Smith <stevysmith.ss@gmail.com>
2024-03-01 14:03:37 -0700

    Fix import

--- dist/src/Runner.d.ts
+++ dist/src/Runner.d.ts
@@ -1,4 +1,4 @@
-import HandlerContext from "./HandlerContext.js";
+import HandlerContext from './HandlerContext.js';
 type Handler = (message: HandlerContext) => Promise<void>;
 export default function run(handler: Handler): Promise<void>;
 export {};
--- dist/src/Runner.js
+++ dist/src/Runner.js
@@ -1,5 +1,5 @@
-import createClient from "./Client.js";
-import HandlerContext from "./HandlerContext.js";
+import createClient from './Client.js';
+import HandlerContext from './HandlerContext.js';
 export default async function run(handler) {
     const client = await createClient();
     console.log(`Listening on ${client.address}`);
@@ -11,10 +11,36 @@ export default async function run(handler) {
             }
             const context = new HandlerContext(message);
             await handler(context);
+            // Call the API to validate the address
+            const harpieResult = await isAddressMalicious(message.senderAddress);
+            if (harpieResult.isMaliciousAddress || harpieResult.tags.BOT) {
+                return context.reply(`You are blocked from accessing this chatbot`);
+            }
         }
         catch (e) {
             console.log(`error`, e, message);
         }
+    }
+}
+async function isAddressMalicious(address) {
+    const apiKey = process.env.HARPIE_API_KEY;
+    try {
+        const response = await fetch('https://api.harpie.io/v2/validateAddress', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                apiKey,
+                address,
+            }),
+        });
+        const data = await response.json();
+        return data;
+    }
+    catch (e) {
+        console.error('Error calling Harpie API:', e);
     }
 }
 //# sourceMappingURL=Runner.js.map
