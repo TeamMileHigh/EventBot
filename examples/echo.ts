4314d07a3c4421899dbd54f1734a0625644ca53f
Steve Smith <stevysmith.ss@gmail.com>
2024-02-29 20:49:50 -0700

    Add subscribing from the bot

--- examples/echo.ts
+++ examples/echo.ts
@@ -1,6 +1,24 @@
-import run from "../src/Runner"
+import run from "../src/Runner";
+import Client from "../src/Client";
+import createClient from "../src/Client";
+
 
 run(async (context) => {
   const messageBody = context.message.content
-  await context.reply(`Welcome to the mile high club: ${messageBody}`)
+  const client = await createClient(); 
+
+  if (messageBody.trim().toLowerCase() === "subscribe") {
+    await client.contacts.refreshConsentList();
+    
+    let state = await client.contacts.consentState(context.message.senderAddress);
+    
+    if (state === "unknown" || state === "denied") {
+      await client.contacts.allow([context.message.senderAddress]);
+      await context.reply("You are now subscribed to receive messages from the bot.");
+    } else {
+      await context.reply("You are already subscribed.");
+    }
+  } else {
+    await context.reply(`Welcome to the mile high club: ${messageBody}`);
+  }
 })
