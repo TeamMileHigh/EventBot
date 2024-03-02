96c5516b3cff6a6b85c7c17eb01e225fb325d81c
Thomas Humphreys <thomasehumphreys@gmail.com>
2024-03-01 19:20:50 -0700

    feat [wip]: story protocol

--- src/Config.ts
+++ src/Config.ts
@@ -1,8 +1,9 @@
-import dotenv from "dotenv"
+import dotenv from 'dotenv';
 
-dotenv.config()
+dotenv.config();
 export const config = {
-  quickNodeApiKey: process.env.QUICKNODE_API_KEY || "",
+  quickNodeApiKey: process.env.QUICKNODE_API_KEY || '',
   apiBaseUrl: 'https://api.quicknode.com/',
-  webhookUrl: process.env.WEBHOOK_HOST_URL || ""
+  webhookUrl: process.env.WEBHOOK_HOST_URL || '',
+  nftContract: '0xE452D6B8873d0ab6A9d1c7a6868Ca5019a2D8213' as `0x${string}`,
 };
--- src/StoryProtocolSetup.ts
+++ src/StoryProtocolSetup.ts
@@ -1,16 +1,32 @@
 import createClient from './Client';
+import { config } from './Config';
 import { ClientType } from './Utils';
+import { ethers } from 'ethers';
+import { abi } from './ABI/ERC721ABI.js';
 import { StoryClient } from '@story-protocol/core-sdk';
 
-export async function registerNft() {
+export async function uploadToIpfs() {}
+
+export async function safeMintNft() {}
+
+export async function registerOnStory() {
   const client = createClient(ClientType.Story);
   const response = await client.ipAsset.registerRootIp({
-    tokenContractAddress: '0xd516482bef63Ff19Ed40E4C6C2e626ccE04e19ED', // your NFT contract address
-    tokenId: '12', // your NFT token ID
+    tokenContractAddress: config.nftContract, // NFT contract address (Sepolia ETH)
+    tokenId: '12',
     txOptions: { waitForTransaction: true },
   });
 
   console.log(
     `Root IPA created at transaction hash ${response.txHash}, IPA ID: ${response.ipId}`
   );
+}
+
+async function getTotalSupply() {
+  const provider = new ethers.AnkrProvider(process.env.RPC_PROVIDER_URL);
+  const contractAddress = config.nftContract;
+  const contract = new ethers.Contract(contractAddress, abi, provider);
+  const mintCount = await contract.mintCounter();
+
+  return mintCount;
 }
