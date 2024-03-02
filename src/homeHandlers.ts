624f4c31c9aae179d235fc28297680a9ce75e568
Steve Smith <stevysmith.ss@gmail.com>
2024-03-02 08:58:33 -0700

    Update lens profile

--- src/homeHandlers.ts
+++ src/homeHandlers.ts
@@ -145,8 +145,8 @@ export async function handleSetupQuickAlerts(context: HandlerContext) {
     if (!profileId) {
       context.reply('You do not have a Lens Profile');
     }
-    // for demo purposes we need a profileId that's not my own https://buttrfly.app/profile/benalistair
-    await createNotification(destination.id, '2743');
+    // for demo purposes we need a profileId that's not my own https://buttrfly.app/profile/jamesfinnerty
+    await createNotification(destination.id, '105878');
     console.log('QuickAlerts setup completed successfully.');
   } catch (error) {
     console.error('Failed to set up QuickAlerts:', error);
