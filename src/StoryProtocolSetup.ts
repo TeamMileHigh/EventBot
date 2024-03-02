21b7957f4198303ccc9daa8661e16a9c328d9f05
Thomas Humphreys <thomasehumphreys@gmail.com>
2024-03-01 19:35:27 -0700

    feat [wip]: story protocol

--- src/StoryProtocolSetup.ts
+++ src/StoryProtocolSetup.ts
@@ -5,9 +5,7 @@ import { ethers } from 'ethers';
 import { abi } from './ABI/ERC721ABI.js';
 import { StoryClient } from '@story-protocol/core-sdk';
 
-export async function uploadToIpfs() {}
-
-export async function safeMintNft() {
+export async function createRootIpa() {
   /** TODO
    * 1. get ipfs hash
    * 2. get totalSupply of minted nfts
@@ -15,11 +13,34 @@ export async function safeMintNft() {
    * 4. register to Root IPA */
 }
 
-export async function registerOnStory() {
+export async function safeMintNft(
+  address: string,
+  tokenId: number,
+  uri: string
+) {
+  const provider = new ethers.AnkrProvider(process.env.RPC_PROVIDER_URL);
+  const contractAddress = config.nftContract;
+  const privateKey = process.env.OWNER_PRIVATE_KEY || '';
+  const wallet = new ethers.Wallet(privateKey, provider);
+  const contract = new ethers.Contract(contractAddress, abi, wallet);
+
+  const tx = await contract.safeMint(address, tokenId, uri);
+  await tx.wait();
+
+  console.log('Minted successfully');
+}
+
+export async function registerOnStory(
+  _tokenId: number,
+  _uri: string,
+  _filename: string
+) {
   const client = createClient(ClientType.Story);
   const response = await client.ipAsset.registerRootIp({
     tokenContractAddress: config.nftContract, // NFT contract address (Sepolia ETH)
-    tokenId: '12',
+    tokenId: String(_tokenId),
+    ipName: _filename,
+    uri: _uri,
     txOptions: { waitForTransaction: true },
   });
 
